f926501f1694564c7fc84cb4ae1bd031377450de
dm03514 <dm03514@gmail.com>
2018-08-02 00:01:28 +0000

    Enables pprof & heap profiles of memory leak

--- simple-memory-leak/main.go
+++ simple-memory-leak/main.go
@@ -1,39 +1,56 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"net/http"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
+	"net/http"
+	_ "net/http/pprof"
 	"sync"
+	"time"
 )
 
 type RequestTracker struct {
-	mu sync.Mutex
-	requests []*http.Request
+	mu       sync.Mutex
+	requests [][]byte
 }
+
 func (rt *RequestTracker) Track(req *http.Request) {
 	rt.mu.Lock()
 	defer rt.mu.Unlock()
-	rt.requests = append(rt.requests, req)
+	// alloc 10KB for each track
+	rt.requests = append(rt.requests, bytes.Repeat([]byte("a"), 10000))
 }
 
-var requests RequestTracker
+var (
+	requests RequestTracker
+
+	responseLatency = prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name: "response_latency_seconds",
+		Help: "Length of HTTP processing.",
+	})
+)
 
 func init() {
 	requests = RequestTracker{}
+	prometheus.MustRegister(responseLatency)
 }
 
-
 func main() {
-	fmt.Print("hello")
+	fmt.Printf("Starting Server on port :%s\n", "8080")
 
 	http.Handle("/metrics", promhttp.Handler())
 	http.HandleFunc("/work", func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
 		requests.Track(r)
 		r.Body.Close()
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`OK`))
+
+		responseLatency.Observe((time.Since(start)).Seconds())
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
