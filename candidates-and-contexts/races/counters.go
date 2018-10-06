9755a98b1b91101afe059b7612e08abdaa97a444
dm03514 <dm03514@gmail.com>
2018-10-06 21:34:36 +0000

    Explict/Implicit Races

---
+++ candidates-and-contexts/races/counters.go
@@ -0,0 +1,46 @@
+package races
+
+import "sync"
+
+type Counter struct {
+	count int
+}
+
+func (c *Counter) Inc() {
+	c.count++
+}
+
+func (c *Counter) Value() int {
+	return c.count
+}
+
+func (c *Counter) Set(v int) {
+	c.count = v
+}
+
+
+type SynchronizedCounter struct {
+	mu *sync.Mutex
+	count int
+}
+
+func (c *SynchronizedCounter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.count++
+}
+
+func (c *SynchronizedCounter) Value() int {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.count
+}
+
+func (c *SynchronizedCounter) Set(v int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.count = v
+}
