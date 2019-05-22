12ed96d2179f80e9460ea7b017fd6ec81598d5ff
Daniel Mican <dm03514@gmail.com>
2019-05-22 11:13:14 -0400

    Contention benchmarks

--- monitor-goroutines/README.md
+++ monitor-goroutines/README.md
@@ -8,24 +8,33 @@ $ go test -bench=.
 goos: darwin
 goarch: amd64
 pkg: github.com/dm03514/grokking-go/monitor-goroutines
-BenchmarkCounterMonitorFromMainGoroutine-12              5000000               292 ns/op
-BenchmarkCounterMonitor1Goroutine-12                     1000000              1022 ns/op
-BenchmarkCounterMonitor10Goroutines-12                   1000000              1101 ns/op
-BenchmarkCounterMonitor100Goroutines-12                  1000000              1232 ns/op
-BenchmarkCounterMonitor1000Goroutines-12                 1000000              1565 ns/op
-BenchmarkCounterMonitor10000Goroutines-12                1000000              1426 ns/op
-BenchmarkCounterMonitorFromMainGoroutineBuffered-12      5000000               261 ns/op
-BenchmarkCounterMonitor1GoroutineBuffered-12             1000000              1081 ns/op
-BenchmarkCounterMonitor10GoroutinesBuffered-12           2000000               931 ns/op
-BenchmarkCounterMonitor100GoroutinesBuffered-12          1000000              1364 ns/op
-BenchmarkCounterMonitor1000GoroutinesBuffered-12         1000000              1237 ns/op
-BenchmarkCounterMonitor10000GoroutinesBuffered-12        1000000              1420 ns/op
-BenchmarkSafeCounterFromMainGoroutine-12                30000000                41.8 ns/op
-BenchmarkSafeCounter1Goroutine-12                        3000000               432 ns/op
-BenchmarkSafeCounter10Goroutine-12                       2000000               672 ns/op
-BenchmarkSafeCounter100Goroutine-12                      2000000               908 ns/op
-BenchmarkSafeCounter1000Goroutine-12                     1000000              1007 ns/op
-BenchmarkSafeCounter10000Goroutine-12                    2000000              1199 ns/op
+BenchmarkCounterMonitorFromMainGoroutine-12                      5000000               297 ns/op
+BenchmarkCounterMonitor1Goroutine-12                             1000000              1009 ns/op
+BenchmarkCounterMonitor10Goroutines-12                           1000000              1100 ns/op
+BenchmarkCounterMonitor100Goroutines-12                          1000000              1262 ns/op
+BenchmarkCounterMonitor1000Goroutines-12                         1000000              1389 ns/op
+BenchmarkCounterMonitor10000Goroutines-12                        1000000              1658 ns/op
+BenchmarkContentionCounterMonitor2Goroutines-12                   500000              2681 ns/op
+BenchmarkContentionCounterMonitor10Goroutines-12                  100000             12665 ns/op
+BenchmarkContentionCounterMonitor100Goroutines-12                  10000            166051 ns/op
+BenchmarkContentionCounterMonitor1000Goroutines-12                  1000           1617492 ns/op
+BenchmarkContentionCounterMonitor10000Goroutines-12                  100          16229725 ns/op
+BenchmarkContentionCounterMonitor2GoroutinesBuffered-12           500000              3071 ns/op
+BenchmarkContentionCounterMonitor10GoroutinesBuffered-12          200000             10944 ns/op
+BenchmarkContentionCounterMonitor100GoroutinesBuffered-12          10000            149916 ns/op
+BenchmarkContentionCounterMonitor1000GoroutinesBuffered-12          1000           1482662 ns/op
+BenchmarkContentionCounterMonitor10000GoroutinesBuffered-12          100          16928763 ns/op
+BenchmarkSafeCounterFromMainGoroutine-12                        30000000                44.3 ns/op
+BenchmarkSafeCounter1Goroutine-12                                3000000               425 ns/op
+BenchmarkSafeCounter10Goroutine-12                               2000000               732 ns/op
+BenchmarkSafeCounter100Goroutine-12                              2000000               992 ns/op
+BenchmarkSafeCounter1000Goroutine-12                             1000000              1255 ns/op
+BenchmarkSafeCounter10000Goroutine-12                            1000000              1248 ns/op
+BenchmarkContentionSafeCounter2Goroutines-12                     1000000              1690 ns/op
+BenchmarkContentionSafeCounter10Goroutine-12                      200000              9074 ns/op
+BenchmarkContentionSafeCounter100Goroutine-12                      10000            114061 ns/op
+BenchmarkContentionSafeCounter1000Goroutine-12                      1000           1225179 ns/op
+BenchmarkContentionSafeCounter10000Goroutine-12                      100          13807604 ns/op
 PASS
-ok      github.com/dm03514/grokking-go/monitor-goroutines       30.996s
+ok      github.com/dm03514/grokking-go/monitor-goroutines       43.980s
 ```
--- monitor-goroutines/main_test.go
+++ monitor-goroutines/main_test.go
@@ -14,7 +14,7 @@ func TestCounterMonitor(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	wg.Add(1)
-	in := NewCounterMonitor(ctx, &wg,0)
+	in := NewCounterMonitor(ctx, &wg, 0)
 	in <- 1
 	close(in)
 }
@@ -107,28 +107,90 @@ func BenchmarkCounterMonitor10000Goroutines(b *testing.B) {
 	benchmarkCounterMonitor(10000, 0, b)
 }
 
-func BenchmarkCounterMonitorFromMainGoroutineBuffered(b *testing.B) {
-	benchmarkCounterMonitor(0, 1, b)
+func benchmarkContentionCounterMonitor(numProducingGoroutines int, channelBufferSize int, b *testing.B) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var counterMonitorWG = sync.WaitGroup{}
+	counterMonitorWG.Add(1)
+	in := NewCounterMonitor(ctx, &counterMonitorWG, channelBufferSize)
+	// fmt.Printf("Num producing goroutines: %d\n", numProducingGoroutines)
+	if numProducingGoroutines <= 1 {
+		panic("need at least 2 goroutines to test contention")
+	}
+
+	// instantiate the correct # of senders
+	chs := []chan struct{}{}
+	var wg = sync.WaitGroup{}
+	for i := 0; i < numProducingGoroutines; i++ {
+		wg.Add(1)
+		triggerSend := make(chan struct{})
+		NewTestSender(
+			ctx,
+			&wg,
+			func() {
+				in <- 1
+			},
+			triggerSend,
+		)
+		chs = append(chs, triggerSend)
+	}
+
+	// we will test b.N writes for EACH goroutine resulting in
+	// and end count of b.N * # goroutines
+	for n := 0; n < b.N; n++ {
+		for _, toSend := range chs {
+			toSend <- struct{}{}
+		}
+	}
+
+	for _, toSend := range chs {
+		close(toSend)
+	}
+
+	wg.Wait()
+	close(in)
+	counterMonitorWG.Wait()
 }
 
-func BenchmarkCounterMonitor1GoroutineBuffered(b *testing.B) {
-	benchmarkCounterMonitor(1, 1, b)
+func BenchmarkContentionCounterMonitor2Goroutines(b *testing.B) {
+	benchmarkContentionCounterMonitor(2, 0, b)
 }
 
-func BenchmarkCounterMonitor10GoroutinesBuffered(b *testing.B) {
-	benchmarkCounterMonitor(10, 10, b)
+func BenchmarkContentionCounterMonitor10Goroutines(b *testing.B) {
+	benchmarkContentionCounterMonitor(10, 0, b)
 }
 
-func BenchmarkCounterMonitor100GoroutinesBuffered(b *testing.B) {
-	benchmarkCounterMonitor(100, 100, b)
+func BenchmarkContentionCounterMonitor100Goroutines(b *testing.B) {
+	benchmarkContentionCounterMonitor(100, 0, b)
 }
 
-func BenchmarkCounterMonitor1000GoroutinesBuffered(b *testing.B) {
-	benchmarkCounterMonitor(1000, 1000, b)
+func BenchmarkContentionCounterMonitor1000Goroutines(b *testing.B) {
+	benchmarkContentionCounterMonitor(1000, 0, b)
 }
 
-func BenchmarkCounterMonitor10000GoroutinesBuffered(b *testing.B) {
-	benchmarkCounterMonitor(10000, 10000, b)
+func BenchmarkContentionCounterMonitor10000Goroutines(b *testing.B) {
+	benchmarkContentionCounterMonitor(10000, 0, b)
+}
+
+func BenchmarkContentionCounterMonitor2GoroutinesBuffered(b *testing.B) {
+	benchmarkContentionCounterMonitor(2, 2, b)
+}
+
+func BenchmarkContentionCounterMonitor10GoroutinesBuffered(b *testing.B) {
+	benchmarkContentionCounterMonitor(10, 10, b)
+}
+
+func BenchmarkContentionCounterMonitor100GoroutinesBuffered(b *testing.B) {
+	benchmarkContentionCounterMonitor(100, 100, b)
+}
+
+func BenchmarkContentionCounterMonitor1000GoroutinesBuffered(b *testing.B) {
+	benchmarkContentionCounterMonitor(1000, 1000, b)
+}
+
+func BenchmarkContentionCounterMonitor10000GoroutinesBuffered(b *testing.B) {
+	benchmarkContentionCounterMonitor(10000, 10000, b)
 }
 
 func benchmarkSafeCounter(numProducingGoroutines int, b *testing.B) {
@@ -202,4 +264,66 @@ func BenchmarkSafeCounter1000Goroutine(b *testing.B) {
 
 func BenchmarkSafeCounter10000Goroutine(b *testing.B) {
 	benchmarkSafeCounter(10000, b)
+}
+
+func benchmarkContentionSafeCounter(numProducingGoroutines int, b *testing.B) {
+	var wg = sync.WaitGroup{}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sc := &SafeCounter{
+		mu: &sync.Mutex{},
+	}
+	if numProducingGoroutines <= 1 {
+		panic("need at least 2 goroutines to test contention")
+	}
+
+	// instantiate the correct # of senders
+	chs := []chan struct{}{}
+	for i := 0; i < numProducingGoroutines; i++ {
+		wg.Add(1)
+		triggerSend := make(chan struct{})
+		NewTestSender(
+			ctx,
+			&wg,
+			func() {
+				sc.Inc()
+			},
+			triggerSend,
+		)
+		chs = append(chs, triggerSend)
+	}
+
+	for n := 0; n < b.N; n++ {
+		for _, toSend := range chs {
+			toSend <- struct{}{}
+		}
+	}
+
+	for _, toSend := range chs {
+		close(toSend)
+	}
+
+	wg.Wait()
+}
+
+func BenchmarkContentionSafeCounter2Goroutines(b *testing.B) {
+	benchmarkContentionSafeCounter(2, b)
+}
+
+func BenchmarkContentionSafeCounter10Goroutine(b *testing.B) {
+	benchmarkContentionSafeCounter(10, b)
+}
+
+func BenchmarkContentionSafeCounter100Goroutine(b *testing.B) {
+	benchmarkContentionSafeCounter(100, b)
+}
+
+func BenchmarkContentionSafeCounter1000Goroutine(b *testing.B) {
+	benchmarkContentionSafeCounter(1000, b)
+}
+
+func BenchmarkContentionSafeCounter10000Goroutine(b *testing.B) {
+	benchmarkContentionSafeCounter(10000, b)
 }
